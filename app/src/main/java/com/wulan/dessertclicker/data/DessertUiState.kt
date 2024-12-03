df5b5b3cfcd95d2cc5b2c740fae02ac41ed104d4
widya <wulandariwidya2005@gmail.com>
2024-12-03 23:45:53 +0700

    Praktikum: Dessert Clicker

---
+++ app/src/main/java/com/wulan/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,12 @@
+package com.wulan.dessertclicker.data
+
+import androidx.annotation.DrawableRes
+import com.wulan.dessertclicker.data.Datasource.dessertList
+
+data class DessertUiState(
+    val currentDessertIndex: Int = 0,
+    val dessertsSold: Int = 0,
+    val revenue: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
+)
